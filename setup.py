30ef9715736889657d83a0ce14f30e5453ad5cff
kirkthaker <kirkthaker66@gmail.com>
2016-03-14 07:31:54 -0400

    prepped for pypi submission

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'investopedia-trading-api',
+  packages = ['investopedia-trading-api'],
+  version = '1.0',
+  description = 'An API for Investopedia\'s paper trading simulator',
+  author = 'Kirk Thaker',
+  author_email = 'kirkthaker66@gmail.com',
+  url = 'https://github.com/kirkthaker/investopedia-trading-api',
+  download_url = 'arburl',
+  keywords = ['trading', 'finance', 'investopedia', 'algorithmic'],
+  classifiers = [],
+)

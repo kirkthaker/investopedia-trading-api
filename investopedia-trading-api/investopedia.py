77737a6abf8320e3b377eec53361a675d024d0b3
Kirk Thaker <kirkthaker66@gmail.com>
2017-06-12 16:04:18 -0400

    Making small changes, set up separate  branch

--- investopedia-trading-api/investopedia.py
+++ investopedia-trading-api/investopedia.py
@@ -4,7 +4,13 @@ from collections import namedtuple
 import re
 import warnings
 
-warnings.filterwarnings("ignore")
+# warnings.filterwarnings("ignore")
+
+Status = namedtuple("Status", "account_val buying_power cash annual_return")
+Portfolio = namedtuple("Portfolio", "bought options shorted")
+Security = namedtuple("Security", "symbol description quantity purchase_price current_price current_value gain_loss")
+Trade = namedtuple("Trade", "date_time description symbol quantity")
+
 
 class Action(Enum):
     buy = 1
@@ -17,11 +23,6 @@ class Duration(Enum):
     day_order = 1
     good_cancel = 2
 
-
-Status = namedtuple("Status", "account_val buying_power cash annual_return")
-Portfolio = namedtuple("Portfolio", "bought options shorted")
-Security = namedtuple("Security", "symbol description quantity purchase_price current_price current_value gain_loss")
-Trade = namedtuple("Trade", "date_time description symbol quantity")
 
 class Account:
     BASE_URL = 'http://www.investopedia.com'
@@ -59,15 +60,15 @@ class Account:
 
         # The ids of all the account information values
         acct_val_id = "ctl00_MainPlaceHolder_currencyFilter_ctrlPortfolioDetails_PortfolioSummary_lblAccountValue"
-        buying_power_id= "ctl00_MainPlaceHolder_currencyFilter_ctrlPortfolioDetails_PortfolioSummary_lblBuyingPower"
+        buying_power_id = "ctl00_MainPlaceHolder_currencyFilter_ctrlPortfolioDetails_PortfolioSummary_lblBuyingPower"
         cash_id = "ctl00_MainPlaceHolder_currencyFilter_ctrlPortfolioDetails_PortfolioSummary_lblCash"
         return_id = "ctl00_MainPlaceHolder_currencyFilter_ctrlPortfolioDetails_PortfolioSummary_lblAnnualReturn"
 
         # Use BeautifulSoup to extract the relevant values based on html ID tags
-        account_value = parsed_html.find('span', attrs={'id':acct_val_id}).text
-        buying_power = parsed_html.find('span', attrs={'id':buying_power_id}).text
-        cash = parsed_html.find('span', attrs={'id':cash_id}).text
-        annual_return = parsed_html.find('span', attrs={'id':return_id}).text
+        account_value = parsed_html.find('span', attrs={'id': acct_val_id}).text
+        buying_power = parsed_html.find('span', attrs={'id': buying_power_id}).text
+        cash = parsed_html.find('span', attrs={'id': cash_id}).text
+        annual_return = parsed_html.find('span', attrs={'id': return_id}).text
 
         # We want our returned values to be floats
         # Use regex to remove non-numerical or decimal characters, but keep - (negative sign)
@@ -104,8 +105,8 @@ class Account:
         stockTable = parsed_html.find('table', attrs={'id': "stock-portfolio-table"}).text
         optionTable = parsed_html.find('table', attrs={'id': "option-portfolio-table"}).text
         shortTable = parsed_html.find('table', attrs={'id': "short-portfolio-table"}).text
-        #the first 20 items are headers and whitespace of the table,
-        #the last 15 items are the totals and whitespace
+        # The first 20 items are headers and whitespace of the table,
+        # The last 15 items are the totals and whitespace
         stockTable_trimmed = stockTable.split("\n")[20:-15]
         optionTable_trimmed = optionTable.split("\n")[20:-15]
         shortTable_trimmed = shortTable.split("\n")[20:-15]
@@ -115,18 +116,18 @@ class Account:
         shortportfolio = []
 
         regexp = "[^0-9.-]"
-        
+
         i = 0
-        while i+1 < len(stockTable_trimmed):
-            symbol=stockTable_trimmed[i]
-            description=stockTable_trimmed[i+1]
-            quantity=float(stockTable_trimmed[i+2])
-            purchase_price=float(re.sub(regexp, '', stockTable_trimmed[i+3])) #take off $ sign
-            current_price=float(re.sub(regexp, '', stockTable_trimmed[i+4]))
-            current_value=float(re.sub(regexp, '', stockTable_trimmed[i+5]))
-            gain_loss=round(((current_price - purchase_price)/purchase_price*100),2)
-        
-            security= Security(
+        while i + 1 < len(stockTable_trimmed):
+            symbol = stockTable_trimmed[i]
+            description = stockTable_trimmed[i + 1]
+            quantity = float(stockTable_trimmed[i + 2])
+            purchase_price = float(re.sub(regexp, '', stockTable_trimmed[i + 3]))  # Take off $ sign
+            current_price = float(re.sub(regexp, '', stockTable_trimmed[i + 4]))
+            current_value = float(re.sub(regexp, '', stockTable_trimmed[i + 5]))
+            gain_loss = round(((current_price - purchase_price) / purchase_price*100), 2)
+
+            security = Security(
                 symbol=symbol,
                 description=description,
                 quantity=quantity,
@@ -147,7 +148,7 @@ class Account:
             current_price=float(re.sub(regexp, '', optionTable_trimmed[i+4]))
             current_value=float(re.sub(regexp, '', optionTable_trimmed[i+5]))
             gain_loss=round(((current_price - purchase_price)/purchase_price*100),2)
-            
+
             security = Security(
                 symbol=symbol,
                 description=description,
@@ -168,7 +169,7 @@ class Account:
             current_price=float(re.sub(regexp, '', shortTable_trimmed[i+4]))
             current_value=float(re.sub(regexp, '', shortTable_trimmed[i+5]))
             gain_loss=round(((current_price - purchase_price)/purchase_price*100 *(-1)),2) # shorted securities rise in value if the stockprice falls
-            
+
             security = Security(
                 symbol=symbol,
                 description=description,
@@ -181,7 +182,7 @@ class Account:
             shortportfolio.append(security)
             i = i + 18
 
-        #Security = namedtuple("Security", "symbol description quantity purchase_price current_price current_value gain_loss(percent)")
+        # Security = namedtuple("Security", "symbol description quantity purchase_price current_price current_value gain_loss(percent)")
 
 
         return Portfolio(
@@ -211,7 +212,7 @@ class Account:
             openTrades.append(trade)
             i=i+12
         return openTrades
-        
+
     def trade(self, symbol, orderType, quantity, priceType="Market", price=False, duration=Duration.good_cancel):
         """
         Executes trades on the platform. See the readme.md file
@@ -231,11 +232,11 @@ class Account:
         trade_form.select("input#symbolTextbox")[0]["value"] = symbol
         trade_form.select("input#quantityTextbox")[0]["value"] = str(quantity)
 
-        #input transaction type
+        # input transaction type
         [option.attrs.pop("selected","") for option in trade_form.select("select#transactionTypeDropDown")[0]("option")]
         trade_form.select("select#transactionTypeDropDown")[0].find("option", {"value":str(orderType.value)})["selected"] = True
 
-        #input price type 
+        # input price type
         [radio.attrs.pop("checked","") for radio in trade_form("input", {"name":"Price"})]
         trade_form.find("input", {"name":"Price", "value": priceType})["checked"]=True
 
@@ -250,9 +251,8 @@ class Account:
         elif price and priceType == "Stop":
             trade_form.select("input#stopPriceTextBox")[0]["value"] = str(price)
 
-
         prev_page = br.submit(trade_form, trade_page.url)
-        prev_form = prev_page.soup.find("form", {"name":"simTradePreview"})
+        prev_form = prev_page.soup.find("form", {"name": "simTradePreview"})
         br.submit(prev_form, prev_page.url)
 
         return True
@@ -264,7 +264,7 @@ def get_quote(symbol):
     Returns the Investopedia delayed price of a given symbol
     """
     br = mechanicalsoup.Browser()
-    response=br.get(BASE_URL+'/markets/stocks/'+symbol.lower())
+    response = br.get(BASE_URL + '/markets/stocks/'+symbol.lower())
     quote_id = "quotePrice"
     parsed_html = response.soup
     try:

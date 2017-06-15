9b29c3e960afd3cdfb3b09fcb7eb8bda2db5f644
Kirk Thaker <kirkthaker66@gmail.com>
2017-06-14 23:42:06 -0400

    More renaming

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'InvestopediaTradingApi',
-  packages = ['InvestopediaTradingApi'],
+  name = 'InvestopediaApi',
+  packages = ['InvestopediaApi'],
   version = '1.0',
   description = 'An API for Investopedia\'s paper trading simulator',
   author = 'Kirk Thaker',
   author_email = 'kirkthaker66@gmail.com',
   url = 'https://github.com/kirkthaker/investopedia-trading-api',
-  download_url = 'https://github.com/kirkthaker/investopedia-trading-api/tarball/0.1',
+  download_url = 'https://github.com/kirkthaker/investopedia-trading-api/tarball/1.0',
   keywords = ['trading', 'finance', 'investopedia', 'algorithmic'],
   classifiers = [],
   install_requires=['mechanicalsoup'],
